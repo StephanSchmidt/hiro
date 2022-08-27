4dd8bb40f367c7f70e267370e093313df37747e0
Stephan Schmidt <stephan@svese.me>
2022-08-27 19:03:19 +0200

    Add test for second  clause

--- compiler/function_test.go
+++ compiler/function_test.go
@@ -44,3 +44,26 @@ end
 	assert.Equal(t, "a", *f.With[0].Equality.Comparison.Addition.Multiplication.Unary.Primary.Variable)
 	assert.Equal(t, 0, *f.With[0].Equality.Comparison.Next.Addition.Multiplication.Unary.Primary.Int)
 }
+
+func TestTwoWith(t *testing.T) {
+	parser := participle.MustBuild[Function](participle.UseLookahead(2))
+	f, _ := parser.ParseString("", `
+fn Double(a:int) with 
+	a>0,
+	a<100
+	-> int:
+	a + a
+end
+`)
+	assert.NotNil(t, f)
+	assert.NotNil(t, f.With)
+	assert.Equal(t, 2, len(f.With))
+	assert.Equal(t, ">", f.With[0].Equality.Comparison.Op)
+	assert.Equal(t, "a", *f.With[0].Equality.Comparison.Addition.Multiplication.Unary.Primary.Variable)
+	assert.Equal(t, 0, *f.With[0].Equality.Comparison.Next.Addition.Multiplication.Unary.Primary.Int)
+
+	assert.Equal(t, "<", f.With[1].Equality.Comparison.Op)
+	assert.Equal(t, "a", *f.With[1].Equality.Comparison.Addition.Multiplication.Unary.Primary.Variable)
+	assert.Equal(t, 100, *f.With[1].Equality.Comparison.Next.Addition.Multiplication.Unary.Primary.Int)
+
+}
