07abbece43b9793a9492efdef50dc0af0de44021
Stephan Schmidt <stephan@svese.me>
2022-08-31 08:52:38 +0200

    Add with type for with expression

--- compiler/with_generator.go
+++ compiler/with_generator.go
@@ -1,28 +1,79 @@
 package compiler
 
-import "github.com/thoas/go-funk"
+import (
+	"github.com/thoas/go-funk"
+)
 
 func IsEqualityOrComparison(e *Expression) bool {
 	return len(e.Equality.Op) > 0 ||
 		(e.Equality.Comparison != nil && len(e.Equality.Comparison.Op) > 0)
 }
 
+type WithType int32
+
+const (
+	Illegal WithType = iota
+	Assertion
+	UnitTest
+	PropTest
+)
+
 type WithExpression struct {
+	WithType          WithType
 	Op                string
 	LeftVars          []string
+	LeftHasVars       bool
+	LeftContainsVar   bool
 	RightVars         []string
+	RightHasVars      bool
+	RightContainsVar  bool
 	LeftContainsCall  bool
 	RightContainsCall bool
 }
 
-func ToWithExpression(rl *RightLeft, funcName string) *WithExpression {
+func WithExpressionTypeFor(f *Function, we *WithExpression) WithType {
+	noCalls := !(we.LeftContainsCall || we.RightContainsCall)
+	noVars := !(we.LeftHasVars || we.RightHasVars)
+	onlyRightCall := we.RightContainsCall && !we.LeftContainsCall
+	onlyLeftCall := !we.RightContainsCall && we.LeftContainsCall
+	// 1>2 // Illegal
+	if noVars && noCalls {
+		return Illegal
+	}
+	//  2 > add(2,1) // Illegal
+	if noVars && onlyRightCall {
+		return Illegal
+	}
+	// add(2,1) == 1 // UnitTest
+	if noVars && onlyLeftCall {
+		return UnitTest
+	}
+	// add(a,b) == a + b // PropTest
+	if we.LeftHasVars && we.LeftContainsCall {
+		return PropTest
+	}
+	// a > 1
+	if !we.RightContainsVar && we.LeftContainsVar && noCalls {
+		return Assertion
+	}
+	return Illegal
+}
+
+func ToWithExpression(rl *RightLeft, funcName string, vars []string) *WithExpression {
+	leftVars := funk.Uniq(rl.LeftVars).([]string)
+	rightVars := funk.Uniq(rl.RightVars).([]string)
 	we := &WithExpression{
-		LeftVars:          funk.Uniq(rl.LeftVars).([]string),
-		RightVars:         funk.Uniq(rl.RightVars).([]string),
+		LeftVars:          leftVars,
+		RightVars:         rightVars,
+		RightHasVars:      len(rightVars) > 0,
+		LeftHasVars:       len(leftVars) > 0,
 		LeftContainsCall:  rl.LeftCalls != nil && funk.Contains(rl.LeftCalls, funcName),
 		RightContainsCall: rl.RightCalls != nil && funk.Contains(rl.RightCalls, funcName),
+		LeftContainsVar:   len(funk.Intersect(leftVars, vars).([]string)) > 0,
+		RightContainsVar:  len(funk.Intersect(rightVars, vars).([]string)) > 0,
 		Op:                rl.Op,
 	}
+
 	return we
 }
 
--- compiler/withexprsssion_test.go
+++ compiler/withexprsssion_test.go
@@ -1,17 +1,97 @@
 package compiler
 
 import (
+	"github.com/alecthomas/participle/v2"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestWithExpression(t *testing.T) {
 	e := expr(`call(a) == b`)
-	we := ToWithExpression(RightLeftVars(e), "call")
+	we := ToWithExpression(RightLeftVars(e),
+		"call",
+		[]string{"a"})
 
 	assert.Equal(t, "==", we.Op)
+	assert.True(t, we.LeftContainsVar)
+	assert.False(t, we.RightContainsVar)
+	assert.True(t, we.LeftHasVars)
+	assert.True(t, we.RightHasVars)
 	assert.Equal(t, "a", we.LeftVars[0])
 	assert.Equal(t, "b", we.RightVars[0])
 	assert.True(t, we.LeftContainsCall)
 	assert.False(t, we.RightContainsCall)
+}
+
+func Func(f string) *Function {
+	parser := participle.MustBuild[Function](participle.UseLookahead(2))
+	fu, _ := parser.ParseString("", f)
+	return fu
+}
+
+func TestUnitTest(t *testing.T) {
+	f := Func(
+		`fn add(a:int, b:int) -> int:
+ 		end`)
+
+	e := expr(`add(2,3) > 2`)
+	we := ToWithExpression(RightLeftVars(e),
+		"add",
+		[]string{"a", "b"})
+	assert.False(t, we.LeftContainsVar)
+	assert.False(t, we.RightContainsVar)
+	assert.False(t, we.LeftHasVars)
+	assert.False(t, we.RightHasVars)
+	assert.True(t, we.LeftContainsCall)
+	assert.False(t, we.RightContainsCall)
+	state := WithExpressionTypeFor(f, we)
+	assert.Equal(t, UnitTest, state)
+}
+
+func TestPropTest(t *testing.T) {
+	f := Func(
+		`fn add(a:int, b:int) -> int:
+ 		end`)
+
+	e := expr(`add(a,b) > a + b`)
+	we := ToWithExpression(RightLeftVars(e),
+		"add",
+		[]string{"a", "b"})
+	assert.True(t, we.LeftContainsVar)
+	assert.True(t, we.LeftHasVars)
+	assert.True(t, we.LeftContainsCall)
+	state := WithExpressionTypeFor(f, we)
+	assert.Equal(t, PropTest, state)
+}
+
+func TestAssertion(t *testing.T) {
+	f := Func(
+		`fn add(a:int, b:int) -> int:
+ 		end`)
+
+	e := expr(`a > 2`)
+	we := ToWithExpression(RightLeftVars(e),
+		"add",
+		[]string{"a", "b"})
+	assert.True(t, we.LeftContainsVar)
+	assert.False(t, we.RightContainsVar)
+	assert.True(t, we.LeftHasVars)
+	assert.False(t, we.RightHasVars)
+	assert.False(t, we.LeftContainsCall)
+	assert.False(t, we.RightContainsCall)
+	state := WithExpressionTypeFor(f, we)
+	assert.Equal(t, Assertion, state)
+}
+
+func TestIllegal(t *testing.T) {
+	f := Func(
+		`fn add(a:int, b:int) -> int:
+ 		end`)
+
+	e := expr(`1>2`)
+	we := ToWithExpression(RightLeftVars(e),
+		"add",
+		[]string{"a", "b"})
+	state := WithExpressionTypeFor(f, we)
+	assert.Equal(t, Illegal, state)
 }
